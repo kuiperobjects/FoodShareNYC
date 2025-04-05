c22b8e6a5a408d9412531b7a04d390a07324934f
kuiperobjects <dha27239@gmail.com>
2025-04-04 23:59:12 -0400

    hi

--- foodshare/foodshareNYC/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import Measurement, Post
-
-
-admin.site.register(Measurement)
-admin.site.register(Post)
